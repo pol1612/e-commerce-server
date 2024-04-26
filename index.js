45a02142c577849c3df011b8cc32c509e5f40f88
Pol Sañé Jové <pol.sane@uvic.cat>
2024-04-26 05:04:02 +0100

    made project starting point

---
+++ index.js
@@ -0,0 +1,35 @@
+const dotenv = require('dotenv');
+const express = require('express'); 
+const mongoose = require('mongoose');
+const cors = require('cors');   
+
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+const MONGO_URI = process.env.mogoDBUri;
+
+const app = express();
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+    }
+);
+
+async function connectToMongoDB() {
+    try {
+        await mongoose.connect((MONGO_URI), { 
+            useNewUrlParser: true, 
+            useUnifiedTopology: true
+        });
+        console.log('Express app connected to MongoDB');
+        app.listen(PORT, () => {
+            console.log(`Express app listening on port ${PORT}`)
+        })            
+    } catch (error) {
+        console.error('Could not connect to MongoDB', error);
+    }
+  }    
+  
+connectToMongoDB();

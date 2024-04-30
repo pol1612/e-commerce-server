f9e0491b8306680cc5fbab7589630b943801cfe5
Pol Sañé Jové <pol.sane@uvic.cat>
2024-04-30 11:40:44 +0100

    made the error http responses work better so that the app doesnt crash if an endpoint is given an incorrect payload

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -19,32 +19,37 @@ const createProduct = async (req, res) => {
     try{
         const createdProduct = await Product.create(newProduct);
         createdProductId = createdProduct['_id']
+
+        if(createdProductId != null){
+            res.send(JSON.stringify(createdProductId.toString()));
+        }else{
+            res.status(400).send({})
+        }
     }
     catch(err){
         res.status(500).send({})
     }
 
-    if(createdProductId != null){
-        res.send(JSON.stringify(createdProductId.toString()));
-    }else{
-        res.status(400).send({})
-    }
+
 }
 
 const getAllProducts = async (req, res) => {
     let productsList
     try {
+
         productsList = await Product.find()
+
+        if(productsList != null){
+            res.send(JSON.stringify(productsList))
+        }else{
+            res.status(400).send({})
+        }
     }
     catch(err){
-        res.status(404).send({})
+        res.status(500).send({})
     }
 
-    if(productsList != null){
-        res.send(JSON.stringify(productsList))
-    }else{
-        res.status(400).send({})
-    }
+
 }
 const getProductById =async (req, res) => {
     const id = req.params.id
@@ -52,53 +57,73 @@ const getProductById =async (req, res) => {
     try {
         product = await Product.findById(id)
 
+        if(product != null){
+
+            res.send(JSON.stringify(product))
+        }else{
+            res.status(404).send({})
+        }
     }catch(err){
-        res.status(404).send({})
+        res.status(500).send({})
     }
 
-    if(product != null){
 
-        res.send(JSON.stringify(product))
-    }else{
-        res.status(400).send({})
-    }
 }
 const updateProduct = async (req, res) => {
     const productId = req.params.id
     const productNewValues = req.body
+    const isUpdateValid = validateProductSchema(productNewValues)
     let updatedProduct
     try{
-        updatedProduct = await Product.findByIdAndUpdate(productId,
-            {
-                productNewValues
+        if (isUpdateValid){
+            updatedProduct = await Product.findByIdAndUpdate(productId,
+                {
+                    productNewValues
+                }
+            )
+
+            if(updatedProduct != null){
+                res.send(JSON.stringify(updatedProduct))
             }
-        )
+            else{
+                res.status(400).send({})
+            }
+        }
+        else{
+            res.status(400).send({})
+        }
 
     }catch (e) {
-        res.status(400).send({})
+        res.status(500).send({})
     }
 
-    if(updatedProduct != null){
-        res.send(JSON.stringify(updatedProduct))
-    }else{
-        res.status(400).send({})
-
-    }
 }
 const deleteProduct = async (req, res) => {
     const productId = req.params.id
     let deletedProduct
     try{
         deletedProduct = await Product.findByIdAndDelete(productId)
+
+        if(deletedProduct != null){
+            res.send(JSON.stringify(deletedProduct))
+        }else{
+            res.status(400).send({})
+        }
+
     }catch (e) {
-        res.status(404).send({})
+        res.status(500).send({})
     }
 
-    if(deletedProduct != null){
-        res.send(JSON.stringify(deletedProduct))
-    }else{
-        res.status(400).send({})
 
+}
+
+function validateProductSchema(productData) {
+    const allowedFields = ['title', 'description', 'price', 'discountPercentage', 'rating', 'stock', 'brand', 'category', 'thumbnail'];
+    const receivedFields = Object.keys(productData);
+    let isValid = false
+    if(receivedFields.length !== 0){
+        isValid = receivedFields.every(field => allowedFields.includes(field));
     }
+    return isValid
 }
 module.exports = { createProduct,getAllProducts, getProductById, updateProduct, deleteProduct}
